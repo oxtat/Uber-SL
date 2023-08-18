2ebfbfab5f9c5c60e25602ae800c64c5d37ef45d
Brian-Wagura <briankiraguri@gmail.com>
2023-08-18 12:11:45 +0300

    Filtered pickups

--- uber_pickups.py
+++ uber_pickups.py
@@ -4,40 +4,49 @@ import pandas as pd
 
 st.title("Uber Pickups")
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATE_COLUMN = "date/time"
+DATA_URL = (
+    "https://s3-us-west-2.amazonaws.com/"
+    "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
 
 
-st.markdown(" load_data is a plain old function that downloads \
+st.markdown(
+    " load_data is a plain old function that downloads \
              some data, puts it in a Pandas dataframe, and converts \
              the date column from text to datetime. \
              The function accepts a single parameter (nrows), which \
              specifies the number of rows that you want to load \
-             into the dataframe.")
+             into the dataframe."
+)
+
 
 @st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns',inplace=True)
+    data.rename(lowercase, axis="columns", inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data_load_state = st.text('Loading data ...')
+
+data_load_state = st.text("Loading data ...")
 data = load_data(10000)
-data_load_state.text('Done! (using st.cache_data)')
+data_load_state.text("Done! (using st.cache_data)")
 
 st.subheader("Raw data")
 st.write(data)
 
 st.subheader("Number of pickups by hour")
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24)
-)[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
 
-#  st.bar_chart() method 
+#  st.bar_chart() method
 st.bar_chart(hist_values)
 
-st.subheader('Map of all pickups')
-st.map(data)
+#  Let's redraw the map to show the concentration of pickups at 17:00.
+# Visualize complex map data,take a look at the st.pydeck_chart.
+
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f"Map of all pickups at {hour_to_filter}:00")
+st.map(filtered_data)
