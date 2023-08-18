3e58aa5b4809f216932748fa64dfa5e1b27ffc63
Brian-Wagura <briankiraguri@gmail.com>
2023-08-18 12:14:00 +0300

    Slider to filter results

--- uber_pickups.py
+++ uber_pickups.py
@@ -45,8 +45,8 @@ st.bar_chart(hist_values)
 
 #  Let's redraw the map to show the concentration of pickups at 17:00.
 # Visualize complex map data,take a look at the st.pydeck_chart.
-
-hour_to_filter = 17
+# Filter results with a slider
+hour_to_filter = st.slider("hour", 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f"Map of all pickups at {hour_to_filter}:00")
 st.map(filtered_data)
